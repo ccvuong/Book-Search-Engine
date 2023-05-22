d0fc165342f3fb4c81a473bc54096a7e77670746
ccvuong <116984891+ccvuong@users.noreply.github.com>
2023-05-21 22:15:30 -0700

    added mutations

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,12 +1,52 @@
+const { AuthenticationError } = require('apollo-server-express');
 const { User, Book } = require('../models');
+const { User } = require("../utils/auth");
+const { signToken } = require('../utils/auth');
 
 const resolvers = {
-  Query: {
+    Query: {
+        me: async () => {
 
-  },
-  Mutation: {
+        },
+    },
 
-  },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+            if (!user) {
+                throw new AuthenticationError("Wrong login information!");
+            };
+
+            const userPW = await user.isCorrectPassword(password);
+            if (!userPW) {
+                throw new AuthenticationError("Wrong password input!");
+            };
+
+            const token = signToken(user);
+            return { token, user };
+        },
+
+        addUser: async (parent, args) => {
+            const user = await User.create(args);
+            const token = signToken(user);
+
+            return { user, token };
+        },
+
+        saveBook: async (parent, { bookData }, context) => {
+            if (context.user) {
+                const updateUser = await User.findOneAndUpdate(
+                    { _id: context.user._id },
+                    { $addToSet: { savedBooks: bookData } },
+                    { new: true },
+                )
+                    .populate("books");
+                return updateUser;
+            };
+            throw new AuthenticationError("Login to save books to your library!")
+        }
+    },
+
 };
 
 module.exports = resolvers;
